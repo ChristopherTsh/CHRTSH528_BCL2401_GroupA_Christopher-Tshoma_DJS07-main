d485634fd986500b6c8e7b9c5f1751f7c3df1a1a
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-31 14:45:38 +0200

    "Updated image source in Header component"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="header">
       {/* Image element with source set to a troll face image */}
-      <img src="./images/troll-face.png" className="header--image" />
+      <img src="./images/WhatsApp Image 2024-05-31 at 14.41.58_fd17b7fa.jpg" className="header--image" />
       {/* Heading element with text "Meme Generator" */}
       <h2 className="header--title">Meme Generator</h2>
       {/* Heading element with text "React Course - DJS07" */}
