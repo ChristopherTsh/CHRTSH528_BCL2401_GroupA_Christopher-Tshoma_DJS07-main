0f0fa754107da4bd8c64b905e3c106cf19bfb315
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-31 14:17:32 +0200

    commented the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import Header from "./components/Header"
-import Meme from "./Meme"
-export default function App() {
+import React from "react";
+import Header from "./components/Header";
+import Meme from "./Meme";
 
+// Exporting a functional component called App
+export default function App() {
+  // Returning the JSX content to be rendered
   return (
     <div>
-            <Header />
-            <Meme />
-        </div>
-  )
+      <Header />
+      <Meme />
+    </div>
+  );
 }
-
-
